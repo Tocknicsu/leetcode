f2e0b5b9f8e9f3da233469a9f36bfb168863e352
Tocknicsu <wingemerald@gmail.com>
2019-12-20 07:56:47 +0000

    feat: Add 100

---
+++ 100/main.go
@@ -0,0 +1,14 @@
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
+// lazy to build tree by hand
